0b4aee3da51c83a5414b626f0a0af312789a9afd
Maxim Kucherov <63538024+MaximKucherov@users.noreply.github.com>
2025-05-20 17:08:16 +0200

    Correct code for provision

--- app/src/main/java/com/example/mydpcproject/MainActivity.java
+++ app/src/main/java/com/example/mydpcproject/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.mydpcproject;
 
+import android.app.admin.DevicePolicyManager;
+import android.content.ComponentName;
 import android.os.Bundle;
+import android.os.Process;
+
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
@@ -8,5 +12,16 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Уведомить Android, что provisioning завершён
+        DevicePolicyManager dpm = (DevicePolicyManager) getSystemService(DEVICE_POLICY_SERVICE);
+        ComponentName admin = new ComponentName(this, MyDeviceAdminReceiver.class);
+
+        if (dpm != null && dpm.isDeviceOwnerApp(getPackageName())) {
+            dpm.setUserProvisioningState(
+                    DevicePolicyManager.STATE_USER_SETUP_COMPLETE,
+                    Process.myUserHandle().hashCode()
+            );
+        }
     }
 }
